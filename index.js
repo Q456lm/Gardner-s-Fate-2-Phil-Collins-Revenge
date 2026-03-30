7969dbec20b2e8cbb74e453dad68460fb41ef3c0
Clarence Bunting <cbbunting1@my.waketech.edu>
2026-03-30 08:46:56 -0400

    This is going to be the hardest easiest thing ever

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+const text1 = document.getElementById("Text1")
+const b1 = document.getElementById("button1")
+const b2 = document.getElementById("button2")
+const b3 = document.getElementById("button3")
+const b4 = document.getElementById("button4")
+const img1 = document.getElementById("image1")
 
+function start(){
+    img1.src = "Collin (1).png"
+    text1.innerHTML = "This strange group has stolen your Drumsticks and Songs and taken them to Zing Zing Zingbah's castle.\nGet them and defeat the enemies."
+    button1.innerHTML = "."
+    button2.innerHTML = "Go Left"
+    button3.innerHTML = "Go Right"
+    button4.innerHTML = "."
+}
+start()
